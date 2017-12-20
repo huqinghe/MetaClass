0cf147c950fb4c9221d5d31ef810c7f8fc5a799b
huqinghe <1178856407@qq.com>
2017-12-20 20:51:48 +0800

    MetaClass理解
    ①object_getClass跟随实例的isa指针，返回此实例所属的类，对于实例对象(instance)返回的是类(class),对于类(class)则返回的是元类(metaclass),
    ②-class方法对于实例对象(instance)会返回类(class),但对于类(class)则不会返回元类(metaclass),而只会返回类本身，即[@”instance” class]返回的是__NSCFConstantString,而[NSString class]返回的是NSString。
    ③class_isMetaClass可判断某类是否为元类.
    
    ④使用objc_allocateClassPair可在运行时创建新的类与元类对，使用class_addMethod和class_addIvar可向类中增加方法和实例变量，最后使用objc_registerClassPair注册后，就可以使用此类了。这体现了OC作为运行时语言的强大之一：在代码运行中动态创建类并添加方法。
    
    eg：
    
    Class newClass = objc_allocateClassPair([NSError class], "RuntimeErrorSubclass", 0);
    class_addMethod(newClass, @selector(addedMethod), (IMP)added_method_implentation, "v@:");
    objc_registerClassPair(newClass);
    
    void added_method_implentation(id self, SEL _cmd)
    {　　
    　　//do something
    }
    1
    2
    3
    4
    5
    6
    7
    8
    说明：objc_allocateClassPair函数的作用是创建一个新类newClass及其元类，三个参数依次为newClass的父类，newClass的名称，第三个参数通常为0。然后可向newClass中添加变量及方法，注意若要添加类方法，需用objc_getClass(newClass)获取元类，然后向元类中添加类方法。接下来必须把newClass注册到运行时系统，否则系统是不能识别这个类的。
    
    收工！

---
+++ MetaClass/MetaClass/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MetaClass
+//
+//  Created by huqinghe on 2017/12/20.
+//  Copyright © 2017年 huqinghe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MetaClass/MetaClass/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MetaClass
+//
+//  Created by huqinghe on 2017/12/20.
+//  Copyright © 2017年 huqinghe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
